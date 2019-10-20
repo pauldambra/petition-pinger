0798e35bf398d1efe1fb38a9e06317833f5cae3f
pauldambra <paul.dambra@gmail.com>
2019-10-20 17:08:34 +0100

    allow the pinger to receive a petition in the URL

--- petition-chooser.js
+++ petition-chooser.js
@@ -1,3 +1,19 @@
+
+const getParameterByName = (queryParam) => {
+  if (URLSearchParams) {
+    const urlParams = new URLSearchParams(window.location.search)
+    return urlParams.get(queryParam)
+  } else {
+    const url = window.location.href
+    queryParam = queryParam.replace(/[\[\]]/g, '\\$&')
+    const regex = new RegExp('[?&]' + queryParam + '(=([^&#]*)|&|#|$)')
+    const results = regex.exec(url)
+    return !results || !results[2]
+      ? ''
+      : decodeURIComponent(results[2].replace(/\+/g, ' '))
+  }
+}
+
 window.Rx.Observable.ajax({
   url: 'https://petition.parliament.uk/petitions.json?state=open',
   crossDomain: true
@@ -13,8 +29,17 @@ window.Rx.Observable.ajax({
   })
 
 var petitionInput = document.getElementById('petition')
+const fromQueryParam = getParameterByName('petition')
+let url
+if (fromQueryParam) {
+  url = fromQueryParam
+  petitionInput.value = fromQueryParam
+} else {
+  url = petitionInput.value
+}
+
 window.Rx.Observable.fromEvent(petitionInput, 'keyup')
-  .startWith(petitionInput.value)
+  .startWith(url)
   .debounceTime(500)
   .subscribe(function (t) {
     window.petitionPinger.url = petitionInput.value
